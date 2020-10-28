9983cfc80c1e478f75738bb61091b83a4859a7e8
Iman Tabrizian <itabrizian@nvidia.com>
2020-10-28 12:57:33 -0400

    Fix multi-line table printing

--- src/table_printer.cc
+++ src/table_printer.cc
@@ -26,46 +26,72 @@
 
 #include "triton/common/table_printer.h"
 
+#include <sys/ioctl.h>
+#include <unistd.h>
 #include <algorithm>
 #include <iomanip>
+#include <iostream>
 #include <memory>
 #include <numeric>
 #include <sstream>
 #include <string>
-#include <sys/ioctl.h>
-#include <unistd.h>
 #include <vector>
-#include <iostream>
 
 namespace triton { namespace common {
 
 //
 // ASCII table printer.
 //
-void TablePrinter::InsertRow(const std::vector<std::string> &row) {
+void
+TablePrinter::InsertRow(const std::vector<std::string>& row)
+{
+  std::vector<std::vector<std::string>> table_row;
+
+  // Number of lines in each field in the record
+  size_t max_height = 0;
+
   // Update max length of data items in each row
   for (size_t i = 0; i < row.size(); ++i) {
-    if (row[i].size() > max_lens_[i])
-      max_lens_[i] = row[i].size();
+    table_row.push_back(std::vector<std::string>{});
+    std::stringstream ss(row[i]);
+    std::string line;
+
+    size_t max_width = 0;
+    while (std::getline(ss, line, '\n')) {
+      table_row[i].push_back(line);
+      if (line.size() > max_width)
+        max_width = line.size();
+    }
+
+    if (max_width > max_widths_[i])
+      max_widths_[i] = max_width;
+
+    size_t number_of_lines = table_row[i].size();
+    if (max_height < number_of_lines)
+      max_height = number_of_lines;
   }
-  data_.emplace_back(row);
+
+  max_heights_.push_back(max_height);
+  data_.emplace_back(table_row);
 }
 
-void TablePrinter::FairShare() {
+void
+TablePrinter::FairShare()
+{
   // initialize original index locations
-  size_t array_size = max_lens_.size();
+  size_t array_size = max_widths_.size();
   std::vector<size_t> idx(array_size);
   iota(idx.begin(), idx.end(), 0);
 
   stable_sort(idx.begin(), idx.end(), [this](size_t i1, size_t i2) {
-    return this->max_lens_[i1] < this->max_lens_[i2];
+    return this->max_widths_[i1] < this->max_widths_[i2];
   });
 
   size_t loop_index = 1;
   for (auto itr = idx.begin(); itr != idx.end(); ++itr) {
     // If a column is not using all the space allocated to it
-    if (max_lens_[*itr] < shares_[*itr]) {
-      float excess = shares_[*itr] - max_lens_[*itr];
+    if (max_widths_[*itr] < shares_[*itr]) {
+      float excess = shares_[*itr] - max_widths_[*itr];
       shares_[*itr] -= excess;
 
       if (itr == idx.end() - 1)
@@ -88,64 +114,96 @@ void TablePrinter::FairShare() {
   for (auto itr = idx.begin(); itr != idx.end(); ++itr) {
     shares_[*itr] = (size_t)shares_[*itr];
   }
+
+  // For each record
+  for (size_t i = 0; i < data_.size(); i++) {
+    auto current_row = data_[i];
+
+    // For each field in the record
+    for (size_t j = 0; j < current_row.size(); j++) {
+      // For each line in the record
+      for (size_t line_index = 0; line_index < current_row[j].size();
+           line_index++) {
+        std::string line = current_row[j][line_index];
+        size_t num_rows = (line.size() + shares_[j] - 1) / shares_[j];
+
+        // If the number of rows required for this record is larger than 1, we
+        // will break that line and put it in multiple lines
+        if (num_rows > 1) {
+          // Remove the multi-line field, it will be replaced by the line
+          // that can fits the column size
+          data_[i][j].erase(data_[i][j].begin() + line_index);
+          for (size_t k = 0; k < num_rows; k++) {
+            size_t start_index =
+                std::min((size_t)(k * shares_[j]), line.size());
+            size_t end_index =
+                std::min((size_t)((k + 1) * shares_[j]), line.size());
+            data_[i][j].insert(
+                data_[i][j].begin() + line_index + k,
+                line.substr(start_index, end_index - start_index));
+          }
+
+          // We need to advance the index for the splitted lines.
+          line_index += num_rows - 1;
+        }
+
+        if (max_heights_[i] < (num_rows - 1 + current_row[j].size()))
+          max_heights_[i] += num_rows - 1;
+      }
+    }
+  }
 }
 
-void TablePrinter::AddRow(std::stringstream &table,
-                            const std::vector<std::string> &row) {
-  size_t max_rows = 0;
+void
+TablePrinter::AddRow(std::stringstream& table, size_t row_index)
+{
+  auto row = data_[row_index];
+  size_t max_height = max_heights_[row_index];
 
-  for (size_t i = 0; i < row.size(); i++) {
-    size_t current_rows = (row[i].size() + shares_[i] - 1) / shares_[i];
-    if (max_rows < current_rows)
-      max_rows = current_rows;
-  }
-  for (size_t j = 0; j < max_rows; j++) {
+  for (size_t j = 0; j < max_height; j++) {
     table << "|" << std::left;
 
     for (size_t i = 0; i < row.size(); i++) {
-      size_t start_index = std::min((size_t)(j * shares_[i]), row[i].size());
-      size_t end_index =
-          std::min((size_t)((j + 1) * shares_[i]), row[i].size());
-
-      // Print part of the column that fits into the fair share of that column
-      table << " " << std::setw(shares_[i])
-            << row[i].substr(start_index, end_index - start_index) << " |";
+      if (j < row[i].size())
+        table << " " << std::setw(shares_[i]) << row[i][j] << " |";
+      else
+        table << " " << std::setw(shares_[i]) << " "
+              << " |";
     }
 
     // Do not add new line if this is the last row of this record
-    if (j != max_rows - 1)
+    if (j != max_height - 1)
       table << "\n";
   }
   table << "\n";
 }
 
-void TablePrinter::AddRowDivider(std::stringstream &table) {
+void
+TablePrinter::AddRowDivider(std::stringstream& table)
+{
   table << "+";
-  for (const auto &share : shares_) {
-    for (size_t i = 0; i < share + 2; i++)
-      table << "-";
+  for (const auto& share : shares_) {
+    for (size_t i = 0; i < share + 2; i++) table << "-";
     table << "+";
   }
   table << "\n";
 }
 
-std::string TablePrinter::PrintTable() {
+std::string
+TablePrinter::PrintTable()
+{
   std::stringstream table;
   table << "\n";
-
-  for (size_t i = 0; i < max_lens_.size(); i++) {
-    if (this->max_lens_[i] < headers_[i].size())
-      max_lens_[i] = headers_[i].size();
-  }
 
   FairShare();
 
   AddRowDivider(table);
-  AddRow(table, headers_);
+  // Add table headers
+  AddRow(table, 0);
   AddRowDivider(table);
 
-  for (size_t j = 0; j < data_.size(); j++) {
-    AddRow(table, data_[j]);
+  for (size_t j = 1; j < data_.size(); j++) {
+    AddRow(table, j);
   }
 
   AddRowDivider(table);
@@ -154,8 +212,8 @@ std::string TablePrinter::PrintTable() {
 }
 
 // TablePrinter will take the ownership of `headers`.
-TablePrinter::TablePrinter(const std::vector<std::string> &headers)
-    : headers_(std::move(headers)) {
+TablePrinter::TablePrinter(const std::vector<std::string>& headers)
+{
   int status = ioctl(STDOUT_FILENO, TIOCGWINSZ, &terminal_size_);
   if (status != 0 || terminal_size_.ws_col == 0) {
     // Failed to get output size
@@ -163,25 +221,26 @@ TablePrinter::TablePrinter(const std::vector<std::string> &headers)
     terminal_size_.ws_col = 500;
   }
 
-  for (size_t i = 0; i < headers_.size(); ++i) {
-    max_lens_.emplace_back(0);
+  for (size_t i = 0; i < headers.size(); ++i) {
+    max_widths_.emplace_back(0);
   }
 
   // Calculate fair share of every column
-  size_t number_of_columns = headers_.size();
+  size_t number_of_columns = headers.size();
 
   // Terminal width is the actual terminal width minus two times spaces
   // required before and after each column and number of columns plus 1 for
   // the pipes between the columns
   size_t terminal_width =
       terminal_size_.ws_col - (2 * number_of_columns) - (number_of_columns + 1);
-  int equal_share = terminal_width / headers_.size();
+  int equal_share = terminal_width / headers.size();
 
-  for (size_t i = 0; i < headers_.size(); ++i) {
+  for (size_t i = 0; i < headers.size(); ++i) {
     shares_.emplace_back(equal_share);
     terminal_width -= equal_share;
   }
+
+  InsertRow(headers);
 }
 
-} // namespace common
-} // namespace triton
+}}  // namespace triton::common
