2b514602866849bf054796260ced0a69a51dba96
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2022-06-01 11:34:53 -0700

    Fix single / double quote

--- src/logging.cc
+++ src/logging.cc
@@ -80,9 +80,9 @@ LogMessage::LogMessage(const char* file, int line, uint32_t level)
     GetSystemTime(&system_time);
     stream_ << level_name_[std::min(level, (uint32_t)Level::kINFO)]
             << std::setfill('0') << std::setw(2) << system_time.wMonth
-            << std::setw(2) << system_time.wDay << " " << std::setw(2)
+            << std::setw(2) << system_time.wDay << ' ' << std::setw(2)
             << system_time.wHour << ':' << std::setw(2) << system_time.wMinute
-            << ':' << std::setw(2) << system_time.wSecond << "." << std::setw(6)
+            << ':' << std::setw(2) << system_time.wSecond << '.' << std::setw(6)
             << system_time.wMilliseconds * 1000 << ' '
             << static_cast<uint32_t>(GetCurrentProcessId()) << ' ' << path << ':'
             << line << "] ";
@@ -93,9 +93,9 @@ LogMessage::LogMessage(const char* file, int line, uint32_t level)
     gmtime_r(((time_t*)&(tv.tv_sec)), &tm_time);
     stream_ << level_name_[std::min(level, (uint32_t)Level::kINFO)]
             << std::setfill('0') << std::setw(2) << (tm_time.tm_mon + 1)
-            << std::setw(2) << tm_time.tm_mday << " " << std::setw(2)
+            << std::setw(2) << tm_time.tm_mday << ' ' << std::setw(2)
             << tm_time.tm_hour << ':' << std::setw(2) << tm_time.tm_min << ':'
-            << std::setw(2) << tm_time.tm_sec << "." << std::setw(6) << tv.tv_usec
+            << std::setw(2) << tm_time.tm_sec << '.' << std::setw(6) << tv.tv_usec
             << ' ' << static_cast<uint32_t>(getpid()) << ' ' << path << ':'
             << line << "] ";
 #endif
@@ -110,7 +110,7 @@ LogMessage::LogMessage(const char* file, int line, uint32_t level)
             << std::setfill('0') << std::setw(2) << system_time.wMonth << '-'
             << std::setw(2) << system_time.wDay << 'T' << std::setw(2)
             << system_time.wHour << ':' << std::setw(2) << system_time.wMinute
-            << ':' << std::setw(2) << system_time.wSecond << 'Z '
+            << ':' << std::setw(2) << system_time.wSecond << "Z "
             << level_name_[std::min(level, (uint32_t)Level::kINFO)] << ' '
             << static_cast<uint32_t>(GetCurrentProcessId()) << ' ' << path << ':'
             << line << "] ";
@@ -123,7 +123,7 @@ LogMessage::LogMessage(const char* file, int line, uint32_t level)
             << std::setfill('0') << std::setw(2) << (tm_time.tm_mon + 1) << '-'
             << std::setw(2) << tm_time.tm_mday << 'T' << std::setw(2)
             << tm_time.tm_hour << ':' << std::setw(2) << tm_time.tm_min << ':'
-            << std::setw(2) << tm_time.tm_sec << 'Z '
+            << std::setw(2) << tm_time.tm_sec << "Z "
             << level_name_[std::min(level, (uint32_t)Level::kINFO)] << ' '
             << static_cast<uint32_t>(getpid()) << ' ' << path << ':'
             << line << "] ";
