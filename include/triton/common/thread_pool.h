1351eca5b793ca9f0180253235b95f3ed40b2a12
Ryan McCormick <rmccormick@nvidia.com>
2022-06-14 15:24:33 -0700

    Pass task as rvalue reference to avoid copy

--- include/triton/common/thread_pool.h
+++ include/triton/common/thread_pool.h
@@ -44,7 +44,7 @@ class ThreadPool {
   using Task = std::function<void(void)>;
   // Assigns "task" to the task queue for a worker thread to execute when
   // available. This will not track the return value of the task.
-  void enqueue(Task task);
+  void enqueue(Task&& task);
 
  private:
   std::queue<Task> task_queue_;
