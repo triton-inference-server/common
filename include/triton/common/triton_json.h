79dc448081fc240e1fb535519ffa336d1b7ec0c3
David Yastremsky <dyastremsky@nvidia.com>
2022-11-17 14:24:37 -0800

    Undefine Windows macro

--- include/triton/common/triton_json.h
+++ include/triton/common/triton_json.h
@@ -26,6 +26,10 @@
 #pragma once
 
 #ifdef _WIN32
+// Remove GetObject definition from windows.h, which prevents calls to
+// RapidJSON's GetObject.
+// https://github.com/Tencent/rapidjson/issues/1448
+#undef GetObject
 #include <rapidjson/document.h>
 #else
 // Disable class-memaccess warning to facilitate compilation with gcc>7
