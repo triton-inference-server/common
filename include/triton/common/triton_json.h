b9a037ff0c5e7045d15927c75e63cb68d9a3fcbe
nnshah1 <neelays@nvidia.com>
2024-05-15 14:30:20 -0700

    remove blank line

--- include/triton/common/triton_json.h
+++ include/triton/common/triton_json.h
@@ -25,7 +25,6 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
-
 #ifdef _WIN32
 // Remove GetObject definition from windows.h, which prevents calls to
 // RapidJSON's GetObject.
