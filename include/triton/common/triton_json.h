82a1897c2df0b6477a0dcdaba87a1fef87c2a064
David Yastremsky <dyastremsky@nvidia.com>
2025-02-26 11:28:32 -0800

    Provide preview of JSON when parse fails
    Clean up formatting

--- include/triton/common/triton_json.h
+++ include/triton/common/triton_json.h
@@ -184,6 +184,11 @@ class TritonJson {
             std::string(GetParseError_En(document_.GetParseError())) + " at " +
             std::to_string(document_.GetErrorOffset())));
       }
+      TRITONJSON_STATUSTYPE status = ParseErrorHandler(document_, std::string(base, size));
+      if (status != TRITONJSON_STATUSSUCCESS) {
+          return status;
+      }
+
       allocator_ = &document_.GetAllocator();
       return TRITONJSON_STATUSSUCCESS;
     }
@@ -192,6 +197,31 @@ class TritonJson {
     TRITONJSON_STATUSTYPE Parse(const std::string& json)
     {
       return Parse(json.data(), json.size());
+    }
+
+    // Helper function for Parse(const char* base, const size_t size) to handle
+    // errors. Return error message if parsing failed.
+    TRITONJSON_STATUSTYPE ParseErrorHandler(
+      const rapidjson::Document& document, const std::string& json)
+    {
+        if (document.HasParseError()) {
+            std::ostringstream error_stream;
+            error_stream << "failed to parse the request JSON buffer: " 
+                        << GetParseError_En(document.GetParseError())
+                        << " at offset " << document.GetErrorOffset() << ".";
+
+            // Show part of the JSON to help debugging
+            const size_t preview_length = 100;
+            std::string json_preview = json.substr(0, preview_length);
+            if (json.size() > preview_length) {
+                json_preview += "...";
+            }
+
+            error_stream << " JSON Preview: \"" << json_preview << "\"";
+
+            return TRITONJSON_STATUSRETURN(error_stream.str());
+        }
+        return TRITONJSON_STATUSSUCCESS;
     }
 
     // Write JSON representation into a 'buffer' in a compact
